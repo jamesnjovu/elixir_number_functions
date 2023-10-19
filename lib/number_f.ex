9f90388ba6e84e0d1552bc270c4b0f8a147347d8
Viper Coder <njovujames@gmail.com>
2023-10-19 09:33:24 +0200

    number_to_delimited

--- lib/number_f.ex
+++ lib/number_f.ex
@@ -12,13 +12,11 @@ defmodule NumberF do
       :world
 
   """
-  def currency(number, unit \\ "$", precision \\ 2) do
-    NumberF.Currency.currency(number, unit, precision)
-  end
+  def currency(number, unit \\ "ZMW", precision \\ 2),
+   do: NumberF.Currency.currency(number, unit, precision)
 
-  def comma_separated(number, precision \\ 2) do
-    NumberF.Currency.comma_separated(number, precision)
-  end
+  def comma_separated(number, precision \\ 2),
+   do: NumberF.Currency.comma_separated(number, precision)
 
   @doc """
   Generate random string based on the given legth. It is also possible to generate certain type of randomise string using the options below:
@@ -30,29 +28,46 @@ defmodule NumberF do
   ## Example
       iex> NumberF.randomizer(20) //"Je5QaLj982f0Meb0ZBSK"
   """
-  def randomizer(length, type \\ :all) do
-    NumberF.Randomizer.randomizer(length, type)
-  end
+  def randomizer(length, type \\ :all),
+   do: NumberF.Randomizer.randomizer(length, type)
 
-  def default_password() do
-    NumberF.Randomizer.gen_password()
-  end
+  def default_password(), do: NumberF.Randomizer.gen_password()
 
   @doc """
   Convert a number into words:
   ## Example
       iex> NumberF.to_words(20.0)
   """
-  def to_words(amount, main_currence \\ "Kwacha", sec_current \\ "Ngwee") do
-    NumberF.NumbersToWords.parse(amount, main_currence, sec_current)
-  end
+  def to_words(amount, main_currence \\ "Kwacha", sec_current \\ "Ngwee"),
+   do: NumberF.NumbersToWords.parse(amount, main_currence, sec_current)
 
   @doc """
   Convert a number into words:
   ## Example
       iex> NumberF.to_words(20.0)
   """
-  def memory_size_cal(size) do
-    NumberF.Memory.humanize(size)
+  def memory_size_cal(size),
+   do: NumberF.Memory.humanize(size)
+
+  def to_int(value) do
+    {nun, _} = Integer.parse(value)
+    nun
   end
+
+  def to_float(value), do: Number.Conversion.to_float(value)
+
+  def to_decimal(value), do: Number.Conversion.to_decimal(value)
+
+  def to_boolean(value) do
+    target = String.downcase(value)
+
+    cond do
+      target == "true" || target == "yes" || target == "on" -> true
+      target == "false" || target == "no" || target == "off" -> false
+      true -> raise ArgumentError, "could not convert #{inspect(value)} to boolean"
+    end
+  end
+
+  def number_to_delimited(number, options \\ []),
+    do: Number.Delimit.number_to_delimited(number, options)
 end
