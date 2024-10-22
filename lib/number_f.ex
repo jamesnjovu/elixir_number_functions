ddb91c436b5c4c3c81f21b9a3aec75bab315dd1c
jamesnjovu <njovujames@gmail.com>
2024-10-23 00:39:54 +0200

    docs added

--- lib/number_f.ex
+++ lib/number_f.ex
@@ -1,63 +1,164 @@
 defmodule NumberF do
-  @moduledoc """
-  Documentation for `NumberF`.
+ @moduledoc """
+  `NumberF` is a utility module for performing various number-related operations such as formatting currency, generating random strings, converting numbers to words, and more.
   """
 
   @doc """
-  Hello world.
+  Formats a number into currency with the specified unit and precision.
+
+  ## Parameters
+    - `number`: The number to format.
+    - `unit`: The currency unit (default: "ZMW").
+    - `precision`: Decimal places (default: 2).
 
   ## Examples
 
-      iex> NumberF.currency()
-      :world
+      iex> NumberF.currency(1234.567)
+      "ZMW 1,234.57"
 
   """
   def currency(number, unit \\ "ZMW", precision \\ 2),
    do: NumberF.Currency.currency(number, unit, precision)
 
+  @doc """
+  Formats a number into comma-separated format with the specified precision.
+
+  ## Parameters
+    - `number`: The number to format.
+    - `precision`: Decimal places (default: 2).
+
+  ## Examples
+
+      iex> NumberF.comma_separated(1234567.89)
+      "1,234,567.89"
+
+  """
   def comma_separated(number, precision \\ 2),
    do: NumberF.Currency.comma_separated(number, precision)
 
+
   @doc """
-  Generate random string based on the given legth. It is also possible to generate certain type of randomise string using the options below:
-  * :all - generate alphanumeric random string
-  * :alpha - generate nom-numeric random string
-  * :numeric - generate numeric random string
-  * :upcase - generate upper case non-numeric random string
-  * :downcase - generate lower case non-numeric random string
-  ## Example
-      iex> NumberF.randomizer(20) //"Je5QaLj982f0Meb0ZBSK"
+  Generates a random string of the specified length.
+
+  ## Parameters
+    - `length`: The length of the string.
+    - `type`: Type of string, with options:
+      - `:all` (alphanumeric)
+      - `:alpha` (alphabetical)
+      - `:numeric` (numbers)
+      - `:upcase` (uppercase)
+      - `:downcase` (lowercase)
+
+  ## Examples
+
+      iex> NumberF.randomizer(10)
+      "a1B2c3D4e5"
+
   """
   def randomizer(length, type \\ :all),
    do: NumberF.Randomizer.randomizer(length, type)
 
+  @doc """
+  Generates a default password with pre-defined complexity.
+
+  ## Examples
+
+      iex> NumberF.default_password()
+      "Dev@2308"
+
+  """
   def default_password(), do: NumberF.Randomizer.gen_password()
 
-  @doc """
-  Convert a number into words:
-  ## Example
+   @doc """
+  Converts a number into words.
+
+  ## Parameters
+    - `amount`: The number to convert.
+    - `main_currence`: The main currency (default: "Kwacha").
+    - `sec_current`: The secondary currency (default: "Ngwee").
+
+  ## Examples
+
       iex> NumberF.to_words(20.0)
+      "Twenty Kwacha and zero Ngwee"
+
   """
   def to_words(amount, main_currence \\ "Kwacha", sec_current \\ "Ngwee"),
    do: NumberF.NumbersToWords.parse(amount, main_currence, sec_current)
 
   @doc """
-  Convert a number into words:
-  ## Example
-      iex> NumberF.to_words(20.0)
+  Converts a memory size to a human-readable format.
+
+  ## Parameters
+    - `size`: The size in bytes.
+
+  ## Examples
+
+      iex> NumberF.memory_size_cal(1048576)
+      "1 MB"
+
   """
   def memory_size_cal(size),
    do: NumberF.Memory.humanize(size)
 
+  @doc """
+  Converts a string to an integer.
+
+  ## Parameters
+    - `value`: The string value to convert.
+
+  ## Examples
+
+      iex> NumberF.to_int("123")
+      123
+
+  """
   def to_int(value) do
     {nun, _} = Integer.parse(value)
     nun
   end
 
+
+  @doc """
+  Converts a value to a float.
+
+  ## Parameters
+    - `value`: The value to convert.
+
+  ## Examples
+
+      iex> NumberF.to_float("123.45")
+      123.45
+
+  """
   def to_float(value), do: Number.Conversion.to_float(value)
 
+  @doc """
+  Converts a value to a decimal.
+
+  ## Parameters
+    - `value`: The value to convert.
+
+  ## Examples
+
+      iex> NumberF.to_decimal("123.45")
+      #Decimal<123.45>
+
+  """
   def to_decimal(value), do: Number.Conversion.to_decimal(value)
 
+  @doc """
+  Converts a string to a boolean value.
+
+  ## Parameters
+    - `value`: The string value to convert. It accepts `true`, `yes`, `on` for true, and `false`, `no`, `off` for false.
+
+  ## Examples
+
+      iex> NumberF.to_boolean("true")
+      true
+
+  """
   def to_boolean(value) do
     target = String.downcase(value)
 
@@ -68,6 +169,37 @@ defmodule NumberF do
     end
   end
 
+  @doc """
+  Formats a number into a delimited format with options for customization.
+
+  ## Parameters
+    - `number`: The number to format.
+    - `options`: Keyword options for formatting (e.g., delimiter, separator).
+
+  ## Examples
+
+      iex> NumberF.number_to_delimited(1234567.89, delimiter: ".", separator: ",")
+      "1.234.567,89"
+
+  """
   def number_to_delimited(number, options \\ []),
     do: Number.Delimit.number_to_delimited(number, options)
+
+  @doc """
+  Sums a list of decimal numbers.
+
+  ## Parameters
+    - `list`: A list of decimal values, potentially nested.
+
+  ## Examples
+
+      iex> sum_decimal([Decimal.new(1.2), Decimal.new(3.4), [Decimal.new(5.6)]])
+      #Decimal<10.2>
+
+  This function flattens any nested lists, then uses `Enum.reduce/3` to sum all the decimal values.
+  """
+  def sum_decimal(list) do
+    List.flatten(list)
+    |> Enum.reduce(Decimal.new(0), &Decimal.add(&1, &2))
+  end
 end
--- mix.exs
+++ mix.exs
@@ -4,10 +4,16 @@ defmodule NumberF.MixProject do
   def project do
     [
       app: :number_f,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "NumberF",
+      source_url: "https://github.com/jamesnjovu/elixir_number_functions",
+      description: "Perform Simple Number Function in elixir",
+      docs: docs(),
+      package: package(),
+      xref: [exclude: [:httpc, :public_key]]
     ]
   end
 
@@ -23,8 +29,21 @@ defmodule NumberF.MixProject do
   defp deps do
     [
       {:number, "~> 1.0.1"},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mveytsman/heroicons_elixir"}
+    ]
+  end
+
+  defp docs do
+    []
   end
 end
