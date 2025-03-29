adb90cecd84074415db6844e6be1a861cb249b6e
James <njovujames@gmail.com>
2025-03-29 13:26:21 +0200

    bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NumberF.MixProject do
   def project do
     [
       app: :number_f,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
